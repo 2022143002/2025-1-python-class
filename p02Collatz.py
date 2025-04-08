68846424264158a37722c7dd8563a636f1d07707
user <yang123123@g.shingu.ac.kr>
2025-04-08 12:25:37 +0900

    세번째 큰 수까지 구하기

--- p02Collatz.py
+++ p02Collatz.py
@@ -13,9 +13,12 @@ n = 9
 # 그 중 가장 큰 수를 찾을 것
 # n=97: 118번 만에 1호 도달
 
-maxvalue = 0
+maxvalue1 = -100
+maxvalue2 = -100
+maxvalue3 = -100
 max1 = 0
 max2 = 0
+max3 = 0
 
 for n in range(1,100):
     print(f'{n=}')
@@ -30,11 +33,18 @@ for n in range(1,100):
         ncount = ncount + 1
 
     print(f'{ncount}')
-    if maxvalue < ncount:
-        maxvalue = ncount
+    if maxvalue1 < ncount:
+        maxvalue2 = maxvalue1
+        maxvalue1 = ncount
+        max2 = max1
         max1 = n
-
-print(f'{maxvalue=}, {max1=}')
+    elif maxvalue2 < ncount:
+        maxvalue2 = ncount
+        max2 = n
+    elif maxvalue3 < ncount:
+        maxvalue3 = ncount
+        max3 = n
+print(f'{maxvalue1=}, {maxvalue2=}, {maxvalue3=}, {max1=}, {max2=}, {max3=}')
 
 
 # for i in range(1,100):
